2f862a3ac5fc9434fc2461ad355f67bd76fe76e8
Okuno Tomoya <gomakuro25@gmail.com>
2020-09-01 16:55:54 +0900

    FIX: scripts
    fix the code to get SCRIPTPATH

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,17 @@
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
+#https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+
+#自分への絶対パス
+#SCRIPT=`readlink \"$0\"`
+#SCRIPT=`realpath $0`
+#
+# 自分がいるディレクトリへの絶対パス
+#SCRIPT="$(abs_dirname "$0")"
+#SCRIPTPATH=`dirname $SCRIPT`
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+
+
+# 画面にパスを表示する
 echo $SCRIPTPATH
 
 g++ -std=c++11 $SCRIPTPATH/../utils/render_balls_so.cpp -o $SCRIPTPATH/../utils/render_balls_so.so -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,13 @@
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
+#SCRIPT=`realpath $0`
+#SCRIPTPATH=`dirname $SCRIPT`
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $SCRIPTPATH/..
+mkdir "data"
+cd "data"
 wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
 unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
-cd -
+cd $SCRIPTPATH/..
+#cd -
